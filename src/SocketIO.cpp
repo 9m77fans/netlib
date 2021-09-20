ed313ebf2cc2e6499e7529bd1f3fed092ca06cbb
wayling <waylingII@gmail.com>
2021-09-20 23:58:25 +0800

    remove commant

--- src/SocketIO.cpp
+++ src/SocketIO.cpp
@@ -51,7 +51,6 @@ int SocketIO::writen(const char *buf, int len) {
   return len - left;
 }
 
-//每一次读取一行数据
 int SocketIO::readline(void *buff, int maxlen) {
   int left = maxlen - 1;
   char *p = (char *)buff;
@@ -59,7 +58,6 @@ int SocketIO::readline(void *buff, int maxlen) {
   int total = 0;
   while (left > 0) {
     ret = recvPeek(p, left);
-    //查找 '\n'
     for (int idx = 0; idx != ret; ++idx) {
       if (p[idx] == '\n') {
         int sz = idx + 1;
@@ -70,25 +68,24 @@ int SocketIO::readline(void *buff, int maxlen) {
         return total;
       }
     }
-    //如果没有发现 '\n'
     readn(p, ret);
     left -= ret;
     p += ret;
     total += ret;
   }
-  *p = '\0';  // 最终没有发现'\n'
+  *p = '\0';
   return total;
 }
 
 int SocketIO::recvPeek(
     char *buf,
-    int len)  //将数据从内核态中复制出来（注意MSG_PEEK代表不删除内核中复制的那部分）
+    int len)
 {
   int ret;
   do {
     ret = recv(_socket.getFd(), buf, len, MSG_PEEK);
   } while (-1 == ret &&
-           errno == EINTR);  //读取多少算多少，但如果遇到系统中断则必须重新读
+           errno == EINTR);
   if (-1 == ret) {
     perror("recv MSG_PEEK");
   }
